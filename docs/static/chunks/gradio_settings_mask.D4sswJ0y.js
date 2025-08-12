f0578ab94b618a2c5c015d3a2cc6991bae6a3264
xiesx <xiesx@qq.com>
2025-08-12 14:16:23 +0800

    更新文档

---
+++ docs/static/chunks/gradio_settings_mask.D4sswJ0y.js
@@ -0,0 +1,1 @@
+const t="/CreatorBox/static/gradio_settings_base.xBHPJywZ.png",s="/CreatorBox/static/gradio_settings_mask.l7EYmPH8.png";export{t as _,s as a};
--- docs/static/chunks/gradio_settings_mask.Dwz4JWoo.js
+++
@@ -1,1 +0,0 @@
-const t="/CreatorBox/static/gradio_settings_base.C8ZDgq4U.png",s="/CreatorBox/static/gradio_settings_mask.l7EYmPH8.png";export{t as _,s as a};
