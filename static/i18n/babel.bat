cf0d6ea3c2ae18b5e1405e6270fbf411cbee5ac7
xiesx <xiesx123@qq.com>
2025-08-26 15:15:09 +0800

    i18n

--- static/i18n/babel.bat
+++ static/i18n/babel.bat
@@ -3,22 +3,22 @@ REM 设置 UTF-8 编码防止中文乱码
 chcp 65001 > nul
 
 echo 正在提取翻译模板...
-pybabel extract -F static/i18n/babel.cfg -o static/i18n/messages.pot .
+pybabel extract --omit-header --sort-output -F static/i18n/babel.cfg -o static/i18n/messages.pot . 
 
 echo 正在更新已有翻译文件...
-pybabel update -i static/i18n/messages.pot -d static/i18n
+pybabel update --omit-header -i static/i18n/messages.pot -d static/i18n 
 
 echo 正在编译 .mo 翻译文件...
-pybabel compile -d static/i18n
+pybabel compile -d static/i18n --use-fuzzy --statistics
 
-@REM REM 是否继续编译
-@REM set /p INPUT=是否编译？输入Y执行，其他跳过： 
+REM 是否继续编译
+set /p INPUT=是否生成？输入Y执行，其他跳过： 
 
-@REM if /I "%INPUT%"=="Y" (
-@REM     echo 正在编译 .mo 翻译文件...
-@REM     pybabel compile -d static/i18n
-@REM ) else (
-@REM     echo 跳过编译步骤
-@REM )
+if /I "%INPUT%"=="Y" (
+    echo 正在生成 Babel 类...
+    python static/i18n/babel.py
+) else (
+    echo 跳过生成步骤
+)
 
 pause
