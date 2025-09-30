32b5c16ed5ac7c20b514836c53e5a1e3d14f8926
xiesx <xiesx123@qq.com>
2025-10-01 00:38:03 +0800

    no message

--- start.bat
+++ start.bat
@@ -1,18 +1,23 @@
 @echo off
 chcp 65001 >nul
 
+REM environment list
+REM echo %Path%
+
 REM Set the conda environment name
 set CONDA_ENV_NAME=creatorbox
 
 REM Set the path of the Python script to run
 set SCRIPT_PATH=cli.py 
 set SCRIPT_ARGS=start -h 0.0.0.0 -p 8000
-
+ 
 REM Initialize conda (for Anaconda/Miniconda)
 CALL conda activate %CONDA_ENV_NAME%
 
-REM Run the Python script
+REM Run authentication command (uncomment and replace <EMAIL> and <PASSWORD> with actual values if needed)
 REM python %SCRIPT_PATH% auth -a login -e <EMAIL> -p <PASSWORD>
+
+REM Run the Python script
 python %SCRIPT_PATH% %SCRIPT_ARGS%
 
 REM Deactivate the conda environment
